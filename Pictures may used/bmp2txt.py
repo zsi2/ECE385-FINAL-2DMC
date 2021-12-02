ecddb0a7d59e27ac98207ea462aed6c05700437e
Yihang He <yihang.19@intl.zju.edu.cn>
2021-12-02 11:04:33 +0800

    Modify bmp2txt and transform alll possible bmp

--- Pictures may used/bmp2txt.py
+++ Pictures may used/bmp2txt.py
@@ -1,13 +1,20 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-img = Image.open('Pictures may used\grass.png')
-pixel_values = list(img.getdata())
-print(len(pixel_values))
 
-with open(r'Pictures may used\txt\grass.txt', 'w') as f:
-    for item in pixel_values:
-        temp = str(item)
-        # f.write(temp)
-        # f.write('\n')
+for root, dirs, files in os.walk(r"Pictures may used\bmp"):
+    for file in files:
+        file_name, file_extension = os.path.splitext(file)
+        if file_extension == '.bmp':
+            img = Image.open(os.path.join(root, file))
+            pixel_values = list(img.getdata())
+            img.close()
+            txt_name = file_name + '.txt'
+            f = open(os.path.join(r'Pictures may used\txt', txt_name), 'w')
+            for item in pixel_values:
+                temp = str(item)
+                f.write(temp)
+                f.write('\n')
+            f.close()
