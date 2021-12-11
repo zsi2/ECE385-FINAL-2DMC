d278cacb3b0a45d5c507fd1e903b60d202bfae67
Yihang He <yihang.19@intl.zju.edu.cn>
2021-12-11 18:29:13 +0800

    add mouse temp files and music files

---
+++ Python code/ReadWAV.py
@@ -0,0 +1,26 @@
+from scipy.io import wavfile
+from scipy.io.wavfile import write
+import numpy as np
+
+def dec2bin(dec):
+    if dec >= 0 and dec <= 32767:
+        return '%.*x' % (4, int(bin(dec), 0))
+    elif dec >= -32768:
+        return '%.*x' % (4, int(bin(dec & 0b1111111111111111), 0))
+    return 'error'
+
+filename = "Music\example.wav"
+
+samplerate, data = wavfile.read(filename)
+
+samplerate, data = wavfile.read(filename)
+left = data[:,0]
+right = data[:,1]
+
+f = open(r"Music\Minecraft_left.txt", 'w')
+g = open(r"Music\Minecraft_right.txt", 'w')
+for i in range(len(left)):
+    data_l = "16'h" + dec2bin(left[i])
+    data_r = "16'h" + dec2bin(right[i])
+    f.write(data_l + '\n')
+    g.write(data_r + '\n')
